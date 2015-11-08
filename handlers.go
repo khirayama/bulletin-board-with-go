53d68e7dca30be4abc67b237657581e01850f0bf
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-11-08 09:58:55 +0900

    Move sql to message model from handlers

--- handlers.go
+++ handlers.go
@@ -14,19 +14,7 @@ func MessageIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 
-	rows, _ := db.Query("SELECT id, name, text FROM message")
-
-	for rows.Next() {
-		var id int
-		var name string
-		var text string
-		rows.Scan(&id, &name, &text)
-		message := Message{}
-		message.Id = id
-		message.Name = name
-		message.Text = text
-		messages = append(messages, message)
-	}
+	messages = getAllMessages()
 	json.NewEncoder(w).Encode(messages)
 }
 
@@ -35,14 +23,11 @@ func MessageCreate(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 
 	json.Unmarshal(body, &message)
-
-	stmt, _ := db.Prepare("INSERT INTO message(name, text) values(?, ?)")
-	stmt.Exec(message.Name, message.Text)
+	createMessage(message)
 }
 
 func MessageDelete(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	messageId := vars["messageId"]
-	stmt, _ := db.Prepare("delete from message where id=?")
-	stmt.Exec(messageId)
+	id := vars["messageId"]
+	deleteMessage(id)
 }
--- message.go
+++ message.go
@@ -7,3 +7,31 @@ type Message struct {
 }
 
 type Messages []Message
+
+func getAllMessages() Messages {
+	var messages Messages
+	rows, _ := db.Query("SELECT id, name, text FROM message")
+
+	for rows.Next() {
+		var id int
+		var name string
+		var text string
+		rows.Scan(&id, &name, &text)
+		message := Message{}
+		message.Id = id
+		message.Name = name
+		message.Text = text
+		messages = append(messages, message)
+	}
+	return messages
+}
+
+func createMessage(message Message) {
+	stmt, _ := db.Prepare("INSERT INTO message(name, text) values(?, ?)")
+	stmt.Exec(message.Name, message.Text)
+}
+
+func deleteMessage(id string) {
+	stmt, _ := db.Prepare("delete from message where id=?")
+	stmt.Exec(id)
+}
