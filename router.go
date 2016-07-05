691f4beaae4d85571dd09cbad9f8b8530f0a0933
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-07-05 18:48:13 +0900

    Create userHandlers

--- router.go
+++ router.go
@@ -8,11 +8,17 @@ import (
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
+	router.HandleFunc("/users", UserCreate).Methods("POST")
+	router.HandleFunc("/users/{id}", UserEdit).Methods("GET")
+	router.HandleFunc("/users/{id}", UserUpdate).Methods("PUT")
+	router.HandleFunc("/users/{id}", UserDelete).Methods("DELETE")
+
 	router.HandleFunc("/messages", MessagesIndex).Methods("GET")
 	router.HandleFunc("/messages", MessageCreate).Methods("POST")
 	router.HandleFunc("/messages/{id}", MessageEdit).Methods("GET")
 	router.HandleFunc("/messages/{id}", MessageUpdate).Methods("PUT")
 	router.HandleFunc("/messages/{id}", MessageDelete).Methods("DELETE")
+
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	http.Handle("/", router)
 	return router
