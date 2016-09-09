68707e6eb59a906b0e722ed47bca475292365bea
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-09 15:38:57 +0900

    Move functions

--- app.go
+++ app.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"database/sql"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/twitter"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
+	"os"
 )
 
 var database, err = sql.Open("sqlite3", "./db/app.db")
+
+func init() {
+	goth.UseProviders(
+		twitter.New(os.Getenv("TWITTER_KEY"),
+			os.Getenv("TWITTER_SECRET"),
+			"http://localhost:8080/auth/twitter/callback?provider=twitter"),
+	)
+}
 
 func main() {
 	log.Print("starting app...")
 	router := NewRouter()
 
-	log.Print("localhost:8080")
+	log.Print("http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- router.go
+++ router.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
-	"github.com/markbates/goth/providers/twitter"
 	"html/template"
 	"net/http"
-	"os"
 )
 
 func NewRouter() *mux.Router {
-	goth.UseProviders(twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://localhost:8080/auth/twitter/callback?provider=twitter"))
-
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", HomeHandler).Methods("GET")
 	router.HandleFunc("/auth/{provider}", gothic.BeginAuthHandler).Methods("GET")
 	router.HandleFunc("/auth/{provider}/callback", sessionsCreate).Methods("GET")
-	router.HandleFunc("/board/", boardHandler).Methods("GET")
-	// router.HandleFunc("/users/{id}", UserEdit).Methods("GET")
-	// router.HandleFunc("/users/{id}", UserUpdate).Methods("PUT")
-	// router.HandleFunc("/users/{id}", UserDelete).Methods("DELETE")
+	router.HandleFunc("/board", boardHandler).Methods("GET")
 
-	// router.HandleFunc("/messages", MessagesIndex).Methods("GET")
-	// router.HandleFunc("/messages", MessageCreate).Methods("POST")
-	// router.HandleFunc("/messages/{id}", MessageEdit).Methods("GET")
-	// router.HandleFunc("/messages/{id}", MessageUpdate).Methods("PUT")
-	// router.HandleFunc("/messages/{id}", MessageDelete).Methods("DELETE")
-
-	// router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	http.Handle("/", router)
 	return router
 }
