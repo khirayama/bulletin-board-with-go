6b15122c397fe73abf23c58f70bc226383998c8d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-09-09 17:22:44 +0900

    Move some lines

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"database/sql"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/providers/twitter"
@@ -26,4 +28,34 @@ func main() {
 
 	log.Print("http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func encrypt(key string, plaintext []byte) []byte {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext
+}
+
+func decript(key string, ciphertext []byte) []byte {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return ciphertext
 }
--- userHandlers.go
+++ userHandlers.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/hex"
-	"fmt"
 	"github.com/markbates/goth/gothic"
 	"net/http"
 	"os"
 )
-
-func encrypt(key string, plaintext []byte) []byte {
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		panic(err)
-	}
-
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-
-	// encrypt
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
-
-	return ciphertext
-}
 
 func sessionsCreate(res http.ResponseWriter, req *http.Request) {
 	user, err := gothic.CompleteUserAuth(res, req)
@@ -36,7 +17,6 @@ func sessionsCreate(res http.ResponseWriter, req *http.Request) {
 
 	encriptKey := os.Getenv("ENCRIPT_KEY")
 	token := hex.EncodeToString(encrypt(encriptKey, make([]byte, currentUser.Id)))
-	fmt.Printf("%x", token)
 
 	http.SetCookie(res, &http.Cookie{
 		Name:  "session",
